b2ce0a3068b4501a171dd7cc26b815432d06fb08
sandra <sandra.tsirk@ut.ee>
2026-03-01 00:00:08 +0200

    feat: add linter

--- src/seb_proovitoo/main.py
+++ src/seb_proovitoo/main.py
@@ -15,7 +15,7 @@ type CurrencyMean = dict[str, float]
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-SELECTED_CURRENCIES: list[str] = ['USD', 'SEK', 'GBP', 'JPY']
+SELECTED_CURRENCIES: list[str] = ["USD", "SEK", "GBP", "JPY"]
 
 
 def main() -> None:
@@ -36,11 +36,14 @@ def fetch_rates() -> tuple[Response, Response]:
     logger.info("Fetching rates...")
 
     try:
-        ecb_rates = httpx.get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip", timeout=30)
+        ecb_rates = httpx.get(
+            "https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip", timeout=30
+        )
         ecb_rates.raise_for_status()
 
-        historical_ecb_rates: Response = httpx.get("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip",
-                                                   timeout=30)
+        historical_ecb_rates: Response = httpx.get(
+            "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip", timeout=30
+        )
         historical_ecb_rates.raise_for_status()
 
         logger.info("Fetching complete.")
@@ -61,11 +64,13 @@ def extract_zip(response: Response) -> CurrencyData:
         filename: str = zip_file.namelist()[0]
 
         with zip_file.open(filename) as csv_file:
-            content: io.TextIOWrapper = io.TextIOWrapper(csv_file, encoding='utf-8')
+            content: io.TextIOWrapper = io.TextIOWrapper(csv_file, encoding="utf-8")
             reader: csv.DictReader[str] = csv.DictReader(content)
 
             for row in reader:
-                cleaned_row: dict[str, str] = {key.strip(): value.strip() for key, value in row.items()}
+                cleaned_row: dict[str, str] = {
+                    key.strip(): value.strip() for key, value in row.items()
+                }
                 for currency in SELECTED_CURRENCIES:
                     value: str = cleaned_row.get(currency)
                     currencies[currency].append(float(value))
@@ -83,8 +88,10 @@ def calculate_mean(data: CurrencyData) -> CurrencyMean:
     return rates
 
 
-def create_html(currency_codes: list[str], rates: CurrencyData, mean: CurrencyMean) -> None:
-    headers: list[str] = ['Currency Code', 'Rate', 'Mean Historical Rate']
+def create_html(
+    currency_codes: list[str], rates: CurrencyData, mean: CurrencyMean
+) -> None:
+    headers: list[str] = ["Currency Code", "Rate", "Mean Historical Rate"]
     table_data: list[list[str | float]] = []
 
     for currency in currency_codes:
@@ -94,11 +101,11 @@ def create_html(currency_codes: list[str], rates: CurrencyData, mean: CurrencyMe
         row: list[str | float] = [currency.upper(), current_rate, historical_mean]
         table_data.append(row)
 
-    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-    output_path = os.path.join(project_root, 'exchange_rates.html')
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    output_path = os.path.join(project_root, "exchange_rates.html")
 
-    with open(output_path, 'w', encoding='utf-8') as f:
-        f.write(tabulate(table_data, headers=headers, tablefmt='html'))
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(tabulate(table_data, headers=headers, tablefmt="html"))
         logger.info(f"HTML table saved to {output_path}")
 
 
